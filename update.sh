eb2385cc65cc8d42b7ee1524d1a65566004f07b3
Jediah Logiodice <jediah@logiodice.com>
2019-07-21 12:43:03 -0400

    Added updates for forced linking, tmux and copy-vi-mode and tmux for osx

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
-ln gitconfig ~/.gitconfig
-ln gdbinit ~/.gdbinit
-ln tmux.conf ~/.tmux.conf
-ln vimrc ~/.vimrc
+ln -f gitconfig ~/.gitconfig
+ln -f gdbinit ~/.gdbinit
+ln -f tmux.conf ~/.tmux.conf
+ln -f vimrc ~/.vimrc
+ln -f tmux-osx.conf ~/.tmux-osx.conf
